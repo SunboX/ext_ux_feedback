02bb126afa1f394250be268ffd9edd4cb8c3c165
André Fiedler <kontakt@visualdrugs.net>
2013-09-09 13:06:29 -0400

    updated Swift Mailer to version 5.0.1

--- lib/swift_mailer/swift_required.php
+++ lib/swift_mailer/swift_required.php
@@ -12,17 +12,20 @@
  * Autoloader and dependency injection initialization for Swift Mailer.
  */
 
-if (defined('SWIFT_REQUIRED_LOADED'))
-	return;
+if (defined('SWIFT_REQUIRED_LOADED')) {
+    return;
+}
 
 define('SWIFT_REQUIRED_LOADED', true);
 
 //Load Swift utility class
 require dirname(__FILE__) . '/classes/Swift.php';
 
-function _swiftmailer_init()
-{
-  require dirname(__FILE__) . '/swift_init.php';
+if (!function_exists('_swiftmailer_init')) {
+    function _swiftmailer_init()
+    {
+        require dirname(__FILE__) . '/swift_init.php';
+    }
 }
 
 //Start the autoloader and lazy-load the init script to set up dependency injection
